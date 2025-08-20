3ee8a272fb8cd031b280c9ccfe353022330554c2
Tim van der Lippe <Tim.Lippe@logius.nl>
2025-08-20 13:45:38 +0200

    Beheer: gebruik nieuwe organisation-config setup
    Part of Logius-standaarden/Automatisering#51

--- js/config.mjs
+++ js/config.mjs
@@ -1,4 +1,6 @@
-var respecConfig = {
+import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
+
+loadRespecWithConfiguration({
   alternateFormats: [ { 
         "label" : "pdf",
         "uri" : "Digikoppeling-Wat-is-Digikoppeling.pdf"
@@ -29,4 +31,4 @@ var respecConfig = {
   shortName: "watisdk",
   specStatus: "WV",
   specType: "HR"
-};
+});
