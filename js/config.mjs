575845c21da773ee9f5fec0685f3326e6b4d865f
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2026-03-09 10:08:02 +0100

    Beheer: verwijder PDF formaat (#14)

--- js/config.mjs
+++ js/config.mjs
@@ -1,10 +1,6 @@
 import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
 
 loadRespecWithConfiguration({
-  alternateFormats: [ { 
-        "label" : "pdf",
-        "uri" : "Digikoppeling-Wat-is-Digikoppeling.pdf"
-      } ],
   authors: [ {
         "name" : "Logius",
         "url" : "https://logius.nl/standaarden"
